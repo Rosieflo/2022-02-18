fcf2d4c5fc3f2d243fc319e962b91bbf60b169c7
Rosieflo <97655399+Rosieflo@users.noreply.github.com>
2022-02-18 14:29:15 +0900

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  $('.s_wrap > li:last-child').insertBefore('.s_wrap > li:first-child');
+  $('.s_wrap').css('margin-left','-1200px');
+
+  function moveleft(){
+    $('.s_wrap').animate({'margin-left':'-2400px'},500,function(){
+      $('.s_wrap > li:first-child').insertAfter('.s_wrap > li:last-child');
+      $('.s_wrap').css('margin-left','-1200px');
+    });
+  }
+
+  let Timer = setInterval(moveleft, 3000);
+
+  let btn = $('.notice > ul > li:first-child > a');
+
+  btn.click(function(){
+    $('.modal').show();
+  });
+
+  let close = $('.c_btn');
+
+  close.click(function(){
+    $('.modal').hide();
+  });
+  
+});
